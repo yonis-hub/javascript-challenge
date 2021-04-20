6e0c816f1dc0e8e8bc23cc6a3f960999454ac9fb
Yonis Hassan <hyonis@fb.com>
2021-04-20 12:57:12 -1000

    handleClick() works

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 console.log("index.js loaded")
 
 // view the ufo data from data.js
-console.log(data)
+// console.log(data)
 
 // get reference to the table body
 var tbody = d3.select("tbody");
@@ -13,11 +13,22 @@ var button = d3.select("#filter-btn");
 // get reference tot he input element on the page
 var inputField = d3.select("#form-control");
 
+// this function is triggered when the button is clicked
+function handleClick() {
+    console.log('Filter button was Clicked!');
+
+    // use d3 to see the object that dispatched 
+    console.log(d3.event.target);
+}
+
+// test the click function 
+button.on('click', handleClick);
+
 
 
 
 // view the ufo data from data.js
-console.log(data)
+// console.log(data)
 
 // forEach loop the data
 data.forEach(function(ufoSightings) {
@@ -28,8 +39,8 @@ data.forEach(function(ufoSightings) {
     Object.entries(ufoSightings).forEach(function([key, value]){
         
         //at this point I have a key and value
-        console.log("looking at new key-value pair")
-        console.log(key,value)
+        // console.log("looking at new key-value pair")
+        // console.log(key,value)
 
         //appending the table 
         var cell = row.append("td")
