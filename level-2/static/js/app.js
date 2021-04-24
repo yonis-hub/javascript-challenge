db3366524aade922fec3f0002f6511e7052d63cf
Yonis Hassan <hyonis@fb.com>
2021-04-24 07:19:37 -1000

    need to fix the bug in level 2

--- level-2/static/js/app.js
+++ level-2/static/js/app.js
@@ -3,17 +3,35 @@ console.log('loading app.js')
 // from data.js
 var tableData = data;
 
-
 // get reference to the button on the page
 var button = d3.select("#filter-btn");
 
 //select the form
 var form = d3.select("#form-control")
 
+//select table body
+var tbody = d3.select('tbody');
 
 // test the click function 
 button.on('click', filterTable);
 form.on('submit', filterTable);
+
+// forEach loop the data and add it the table
+tableData.forEach(function(ufoSightings) {
+    
+    // console.log(ufoSightings);
+    
+    var row = tbody.append("tr")
+
+    Object.entries(ufoSightings).forEach(function([key, value]){
+        
+        //appending the table 
+        var cell = row.append("td")
+        cell.text(value);
+
+    });
+});
+
 
 // this function is triggered when the filter table 
 function filterTable() {
@@ -21,29 +39,35 @@ function filterTable() {
     // prevent the page from refreshing
     d3.event.preventDefault();
 
-    //select input element
-    var inputElement =d3.select('#Date');
+    //select input element and convert input lowercase
+    var inputDate = d3.select('#Date').property("value");
+    var inputCity = d3.select("#City").property("value").toLowerCase().trim();
+    var inputState = d3.select("#State").property("value").toLowerCase().trim();
+    var inputCountry = d3.select("#Country").property("value").toLowerCase().trim();
+    var inputShape = d3.select("#Shape").property("value").toLowerCase().trim();
 
-    //get the value property of the input element
-    var inputValue = inputElement.property('value');
+    //filter based on `date, city, state, country and shape
+    var dataFilter = tableData;
+    
+    if (inputDate) {
+        dataFilter = dataFilter.filter(tableData => tableData.datetime === inputDate);
+    }
 
-    //view in the input log
-    console.log('input', inputValue);
+    if (inputCity) {
+        dataFilter = dataFilter.filter(tableData => tableData.datetime === inputCity);
+    }
+    
+    if (inputState) {
+        dataFilter = dataFilter.filter(tableData => tableData.datetime === inputState);
+    }
 
-    //filter based on `date/time` , `city`, `state`, `country` and `shape`
-    var dataFilter = tableData.filter(tableData => tableData.datetime === inputValue);
-    console.log(dataFilter)
+    if (inputCountry) {
+        dataFilter = dataFilter.filter(tableData => tableData.datetime === inputCountry);
+    }
 
-    // var cityFilter = tableData.filter(tableData => tableData.city === inputValue);
-
-    // var stateFilter = tableData.filter(tableData => tableData.state === inputValue);
-
-    // var countryFilter = tableData.filter(tableData => tableData.country === inputValue);
-
-    // var shapeFilter = tableData.filter(tableData => tableData.shape === inputValue);
-
-    //select table body
-    var tbody = d3.select('tbody');
+    if (inputShape) {
+        dataFilter = dataFilter.filter(tableData => tableData.datetime === inputShape);
+    }
 
     //clear the table body with empyt string
     tbody.html("");
@@ -60,23 +84,5 @@ function filterTable() {
         });
     });
 };
-
-
-// forEach loop the data and add it the table
-
-tableData.forEach(function(ufoSightings) {
-    
-    // console.log(ufoSightings);
-    
-    var row = tbody.append("tr")
-
-    Object.entries(ufoSightings).forEach(function([key, value]){
-        
-        //appending the table 
-        var cell = row.append("td")
-        cell.text(value);
-
-    });
-});
 
 
